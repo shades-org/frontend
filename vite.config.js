6d7d255e711638613377693ee0002132f1cb9754
Krishnendu Dasgupta <krishnendu.kdg@gmail.com>
2024-01-29 01:25:25 +0530

    Initial Project Setup

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
